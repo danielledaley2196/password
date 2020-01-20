ad84ef85a1bb04dd34c6a850ed9108b1f4185a33
Danielle Daley <danielledaley2196@gmail.com>
2020-01-20 18:05:38 -0500

    proper linking is a thing

--- Develop/script.js
+++ Develop/script.js
@@ -16,24 +16,26 @@ let chkLowerCase = document.getElementById("chkLowerCase");
 let chkNumbers = document.getElementById("chkNumbers");
 let chkSpecialChars = document.getElementById("chkSpecialChars");
 
-if (chkUpperCase.checked) {
-  chkUpperCase += upperCase;
-}
-if (chkLowerCase.checked) {
-  chkLowerCase += lowerCase;
-}
-if (chkNumbers.checked) {
-  chkNumbers += numerals;
-}
-if (chkSpecialChars.checked) {
-  chkSpecialChars += specialChars;
-}
 
-let passLength = document.getElementById(howLong);
-console.log(passLength);
+let passLength = parseInt(document.getElementById(howLong));
+
 
 
 let generatePassword = function() { 
+  if (chkUpperCase.checked) {
+    chkUpperCase += upperCase;
+    console.log(chkUpperCase);
+  }
+
+  if (chkLowerCase.checked) {
+    chkLowerCase += lowerCase;
+  }
+  if (chkNumbers.checked) {
+    chkNumbers += numerals;
+  }
+  if (chkSpecialChars.checked) {
+    chkSpecialChars += specialChars;
+  }
   for (i=0; i < passLength; i++) {
     if //create your loop here please tieing everything together 
     (i <= 0) {
@@ -63,11 +65,15 @@ function writePassword() {
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 
-  return passwordText;
+  return password;
 }
 
 function copyToClipboard() {
   // BONUS 
+  let copyTo = document.getElementById("copyMe");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
 }
 
 // Add event listener to generate button
